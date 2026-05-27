f0018d5740e36965a87de1b36e449cbd4d0f2ff0
Rawal Andrays <rawalbusiness733@gmail.com>
2026-05-27 19:35:48 +0500

    Add Header component with styling and lucide-react dependency

--- src/components/section1/header.jsx
+++ src/components/section1/header.jsx
@@ -1,8 +1,26 @@
-import "../../styles/header.module.css";
+import style from "../../styles/header.module.css";
+import Logo from "../../assets/logo.png";
+
 const Header = () => {
   return (
-    <div>
-      
+    <div className={style.main}>
+      <div className={style.left}>
+        <img src={Logo}/>
+        <h4>/ Sales@agent.ai</h4>
+      </div>
+      <div className={style.mid}>
+        <p>Product</p>
+        <span className={style.dot}></span>
+        <p>Solutions</p>
+        <span className={style.dot}></span>
+        <p>Pricing</p>
+        <span className={style.dot}></span>
+        <p>Developers</p>
+      </div>
+      <div className={style.right}>
+        <h4>Log in</h4>
+        <button>Get it Now - It's Free</button>
+      </div>
     </div>
   )
 }
